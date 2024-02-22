b60b405126341536042e2cb54d8beaeeac40afda
LavanyaNair604 <74152673+LavanyaNair604@users.noreply.github.com>
2024-02-22 21:26:08 +0530

    image path again

--- STREAMLIT/Home.py
+++ STREAMLIT/Home.py
@@ -14,7 +14,7 @@ col1, col2 = st.columns(2)
     
 
 with col1:
-    st.image("/STREAMLIT/pages/CAFESS.jpg")
+    st.image("\STREAMLIT\pages\CAFESS.jpg")
     
 with col2:
     st.header(" Coffee and Snacks is a beautiful combination! :cookie:")
