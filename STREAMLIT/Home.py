366b56dd6d18535d7eeb869f941fb8b9bbc34933
LavanyaNair604 <74152673+LavanyaNair604@users.noreply.github.com>
2024-02-22 19:34:53 +0530

    Update Home.py

--- STREAMLIT/Home.py
+++ STREAMLIT/Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+# from langchain.llms import OpenAI
 import time
 st.set_page_config(page_title="Cheruby Greens Cafe",layout="wide",initial_sidebar_state="collapsed")
 st.header("Cheruby Greens Cafe :coffee:", divider="green")
@@ -13,7 +14,7 @@ col1, col2 = st.columns(2)
     
 
 with col1:
-    st.image("STREAMLIT/pages/CAFESS.jpg")
+    st.image("\STREAMLIT\pages\CAFESS.jpg")
     
 with col2:
     st.header(" Coffee and Snacks is a beautiful combination! :cookie:")
@@ -24,3 +25,16 @@ with col2:
     st.page_link("pages/2_Different_Snacks.py", label=":green[Explore snacks]",icon = "🍩")
     st.page_link("pages/3_Order_Items.py", label=":green[Order]",icon = "📝")
 
+#     openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+# def generate_response(input_text):
+#     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+#     st.info(llm(input_text))
+
+# with st.form('my_form'):
+#     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+#     submitted = st.form_submit_button('Submit')
+#     if not openai_api_key.startswith('sk-'):
+#         st.warning('Please enter your OpenAI API key!', icon='⚠')
+#     if submitted and openai_api_key.startswith('sk-'):
+#         generate_response(text)
